f2d42931d7f04001e474005db36f1183f3bccdc0
MohamedHegab <mohamedhegab92@gmail.com>
2016-02-25 13:31:42 +0200

    Updates application controller to prevent CSRF exception from being raised

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe User do
 
   subject { @user }
 
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation)}
+  it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation)}
 
-  it { should be_valid }
+  it { expect(@user).to be_valid }
 
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
-	it { should validate_confirmation_of(:password) }
-	it { should allow_value('example@domain.com').for(:email) }
+  it { expect(@user).to validate_presence_of(:email) }
+  it { expect(@user).to validate_uniqueness_of(:email) }
+	it { expect(@user).to validate_confirmation_of(:password) }
+	it { expect(@user).to allow_value('example@domain.com').for(:email) }
 end
